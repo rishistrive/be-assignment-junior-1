6a1688da822e823719651735838fd33b3baee09a
rishistrive <rishi.strive@gmail.com>
2024-05-01 22:09:21 +0530

    added routes for controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  # get 'expenses/index'
+  # get 'expenses/new'
+  # post 'expenses/create'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :expenses do
+    patch 'settle_up', on: :collection
+  end
+  # get 'add_item', to: 'expenses#add_item'
   root to: "static#dashboard"
   get 'people/:id', to: 'static#person'
 end
