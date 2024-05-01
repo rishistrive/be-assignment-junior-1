a93b3e9b1ced84f907b8fd4be571bab2102dda79
rishistrive <rishi.strive@gmail.com>
2024-05-01 22:06:58 +0530

    added logic for you owe,owed and dashboard logic

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,14 @@
 class StaticController < ApplicationController
   def dashboard
+    @expense = Expense.new
+    @users = User.all
+    @user_expenses = @users.map { |user| [user.id, user.expenses] }.to_h
+    @total_your_are_owed = Expense.owed_to_user(current_user.id).round(2) rescue 0
+    @total_you_owe = Expense.owed_by_user(current_user.id).round(2) rescue 0
+    @total_balance = Expense.balance_for_user(current_user.id).round(2)
+    @settle_up = User.settle_up_for_user(current_user.id) rescue nil
+    @you_owe_to_other = User.you_owe_to_other(current_user.id) rescue nil
+    @you_owed_by_other = User.you_owed_by_other(current_user.id) rescue nil
   end
 
   def person
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,37 @@ class User < ApplicationRecord
   has_many :participants
   has_many :expenses
   has_many :paid_expenses, foreign_key: :paid_by, class_name: 'Expense'
+
+  scope :with_participants_and_paid_by_user, ->(user_id) {
+    joins(participants: { expense: :paid_by_user })
+      .where(participants: { user_id: user_id })
+  }
+
+  scope :settle_up_for_user, ->(user_id) {
+    with_participants_and_paid_by_user(user_id)
+      .where.not(expenses: { paid_by: user_id })
+      .where(participants: { paid: false })
+      .pluck('"paid_by_users_expenses"."name"', 'participants.share_amount', 'participants.paid', 'participants.user_id')
+      .map do |name, share_amount, paid, user_id|
+        {
+          name: name,
+          share_amount: share_amount,
+          paid: paid,
+          user_id: user_id
+        }
+      end
+  }
+
+  scope :you_owe_to_other, ->(user_id) {
+    with_participants_and_paid_by_user(user_id)
+      .where.not(expenses: { paid_by: user_id })
+      .pluck('"paid_by_users_expenses"."name"', 'participants.share_amount', 'participants.paid')
+  }
+
+  scope :you_owed_by_other, ->(user_id) {
+    joins(participants: :expense)
+      .where(expenses: { paid_by: user_id })
+      .where.not(participants: { user_id: user_id })
+      .pluck(:name, :share_amount, 'participants.paid')
+  }
 end
